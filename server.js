083c1533874bf317f8c4452958ade4cc36f23ef7
clearbusinessinsight <choltz@cacbydesign.com>
2022-11-30 17:36:36 -0500

    changes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/notes', require('./middleware/api'));
 app.use('/', require('./middleware/html'));
 
 // start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}....`);
